ccecf675869697bba8dda51700bad4321afaba50
赖承海 <179455570@qq.com>
2015-11-25 17:53:59 +0800

    细节补充

--- demo-list/work10/js/base.js
+++ demo-list/work10/js/base.js
@@ -7,7 +7,12 @@ $(function(){
         downColor:'#ccc',
         upColor:'#ccc'
     });
-
+    $('.img-tab').find('a').click(function(){
+        if($(this).hasClass('select-tab')){
+            return 0;
+        }
+        $('.img-tab').find('a').toggleClass('select-tab');
+    });
 
 
 
