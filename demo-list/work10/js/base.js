49dfb7ffefa39162d586fcfe256cbbc8ff9ef5ff
赖承海 <179455570@qq.com>
2015-11-26 10:44:53 +0800

    11

--- demo-list/work10/js/base.js
+++ demo-list/work10/js/base.js
@@ -12,6 +12,30 @@ $(function(){
             return 0;
         }
         $('.img-tab').find('a').toggleClass('select-tab');
+        $('.race-way-img-box').find('img').toggleClass('hidden');
+        $('.src-data').toggleClass('init');
+    });
+    var d = dialog({
+        width:500,
+        height:600,
+        content:'<img id="big-img" src="img/test-way-img.png">'
+    });
+
+
+    $('.look-big-img').click(function(){
+        var img = $('#big-img');
+        if(img.hasClass('spic')){
+            d.close();
+            img.removeClass('spic');
+
+        }else{
+            var src = "img/"+$('.init').text();
+            img.addClass('spic');
+            img.attr({'src':src});
+            d.show();
+
+        }
+
     });
 
 
