2f28553425bc28c2c331649531db5c879755b4b8
赖承海 <179455570@qq.com>
2015-12-03 14:18:01 +0800

    增加弹出层效果

---
+++ demo-list/work11/js/base.js
@@ -0,0 +1,17 @@
+$(function(){
+    console.log(111);
+    var d = dialog({
+        width:960,
+        height:919,
+        content:'<img id="big-img" src="img/1.jpg"><div id="img-close" class="close-btn"></div>'
+    });
+    //d.showModal();
+    $('.img-item').find('img').click(function(){
+        var src = $(this).attr('src');
+        $('#big-img').attr({'src':src});
+        d.showModal();
+    });
+    $('.close-btn').click(function(){
+        d.close();
+    })
+});
