388abcb55864602ee4fc72e85360e1b5a779b28b
赖承海 <179455570@qq.com>
2016-03-14 13:44:04 +0800

    tuzi

--- demo-list/work10/js/mobile.js
+++ demo-list/work10/js/mobile.js
@@ -50,14 +50,49 @@ $(function(){
                 }else if(type==='Android'){
                     window.ANDROID.didClickImageViewFromWebView( JSON.stringify({'imagesList': json.imagesList, 'currentImage': json.currentImage}) );
                 }else{
-                    console.log(111);
+                    //console.log(111);
                 }
 
             } catch (error) {
-                console.log('imagesList: ' + json.imagesList, 'currentImage: ' + json.currentImage);
+                //console.log('imagesList: ' + json.imagesList, 'currentImage: ' + json.currentImage);
             }
 
         }
     };
+    var delegateHrefHandel = function(event){
+        event.preventDefault();
+
+        var me = $(this);
+        var href = me[0].href;
+        var json = {};
+        if (me[0].tagName == 'A') {
+            var type =$('#os').val();//type=1: IOS ;type=2:AND
+
+            json.href = href;
+
+            try {
+                //根据类型判断调用
+                if(type==='iOS'){
+                    Bridge.connect(function (bridge) {
+                        bridge.callHandler('didClickHrefViewFromWebView', {
+                            'href': json.href
+                        })
+                    });
+                }else if(type==='Android'){
+                    window.ANDROID.didClickHrefViewFromWebView( JSON.stringify({'href': json.href}) );
+                }else{
+                    //console.log(111);
+                }
+
+            } catch (error) {
+                //console.log('href: ' + json.href);
+            }
+
+
+
+        }
+    };
+
     $('.info-text').delegate('img', 'click',delegateHandle);
+    $('.info-text').delegate('a', 'click',delegateHrefHandel);
 });
